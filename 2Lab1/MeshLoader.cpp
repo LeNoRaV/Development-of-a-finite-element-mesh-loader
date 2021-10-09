c839dc1f1f2c6cc036d978915ac7b42361807b3a
Илья Богданов <ibogdanov@bmstu.ru>
2021-10-09 16:49:38 +0000

    Замечания по MeshLoader.cpp

--- 2Lab1/MeshLoader.cpp
+++ 2Lab1/MeshLoader.cpp
@@ -32,6 +32,8 @@ std::vector<int> MeshLoader::find_elem_id(int p_node_id_1, int p_node_id_2, int 
 					&& std::find(elem.m_node_id.begin(), elem.m_node_id.end(), p_node_id_3) != elem.m_node_id.end();
 			});
 
+
+        //!!! Ниже что-то сложное - упростите
 		first_to_find = found_elem;
 		if (found_elem != last_to_find) {
 			res.push_back(found_elem->m_element_id);
@@ -55,6 +57,7 @@ std::vector<int> MeshLoader::find_elem_edge(int p_node_id_1, int p_node_id_2) co
 					&& std::find(elem.m_node_id.begin(), elem.m_node_id.end(), p_node_id_2) != elem.m_node_id.end();
 			});
 
+        //!!! Ниже что-то сложное - упростите
 		first_to_find = found_elem;
 		if (found_elem != last_to_find) {
 			res.push_back(found_elem->m_element_id);
@@ -78,10 +81,11 @@ std::vector<int> MeshLoader::get_node_by_boundary(int p_boundary_id) const
 				return cur_boundary.m_boundary_id == p_boundary_id;
 			});
 
+        //!!! Ниже что-то сложное - упростите
 		first_to_find = found_boundary;
 		if (found_boundary != last_to_find) {
 			for (const auto& node_in_boundary : found_boundary->m_node_id)
-				res.push_back(node_in_boundary);
+				res.push_back(node_in_boundary);                //!!! Где гарантия, что ID не будут дублироваться в res? Тип контейнера выбран неверно.
 			++first_to_find;
 		}
 	}
@@ -101,10 +105,11 @@ std::vector<int> MeshLoader::get_node_by_finite(int p_material_id) const
 				return cur_finite.m_material_id == p_material_id;
 			});
 
+        //!!! Ниже что-то сложное - упростите
 		first_to_find = found_finite_elem;
 		if (found_finite_elem != last_to_find) {
 			for (const auto& node_in_finite_elem : found_finite_elem->m_node_id)
-				res.push_back(node_in_finite_elem);
+				res.push_back(node_in_finite_elem);     //!!! Где гарантия, что ID не будут дублироваться в res? Тип контейнера выбран неверно.
 			++first_to_find;
 		}
 	}
@@ -129,7 +134,7 @@ std::vector<int> MeshLoader::get_f_node_by_boundary(int p_boundary_id) const
 		if (found_boundary != last_to_find) {
 			for (const auto& node_in_boundary : found_boundary->m_node_id) {
 				if (m_nodes.at(node_in_boundary).m_flag == 0)
-					res.push_back(node_in_boundary);
+					res.push_back(node_in_boundary);      //!!! Где гарантия, что ID не будут дублироваться в res? Тип контейнера выбран неверно.
 			}
 			++first_to_find;
 		}
@@ -139,18 +144,22 @@ std::vector<int> MeshLoader::get_f_node_by_boundary(int p_boundary_id) const
 
 void MeshLoader::insert_middle() 
 {
+    //!!! Что не понятное тут проиходит...
+    
 	for (auto& elem : m_finite_elems) {
 		std::vector<int> cur_nodes_id = elem.m_node_id;
 		for (auto first_node = 0; first_node < 4; ++first_node)
 			for (auto second_node = first_node + 1; second_node < 4; ++second_node) {
+			    
 				Edge cur_edge(cur_nodes_id[first_node], cur_nodes_id[second_node]);
-				if (cur_edge.m_middle_node==-1) {
+				
+				if (cur_edge.m_middle_node==-1) { //!!! Не очень понятно, у только что созданного ребра может быть не -1 ?
 					Node new_node = get_middle_node(cur_edge);
-					cur_edge.update_mid(new_node.m_node_id);
+					cur_edge.update_mid(new_node.m_node_id); //!!! И какой смысл? В конце итерации cur_edge удаляется.
 					m_nodes.push_back(new_node);
 				}
 				else {
-					elem.m_node_id.push_back(cur_edge.m_middle_node);
+					elem.m_node_id.push_back(cur_edge.m_middle_node); //!!! А кто инициализировал m_middle_node и когда?
 				}
 			}
 	}
@@ -180,7 +189,7 @@ std::vector<std::vector<int>> MeshLoader::get_adjacent_nodes() const
 		for (auto cur_node_id : cur_elem.m_node_id)
 			for (auto adj_node_id : cur_elem.m_node_id)
 				if (cur_node_id != adj_node_id)
-					res[cur_node_id].push_back(adj_node_id);
+					res[cur_node_id].push_back(adj_node_id); //!!! Где гарантия, что ID не дублируются? Контейнер выбран неверно.
 	}
 
 	return res;
