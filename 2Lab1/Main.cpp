04ea68fc9746c7d9b1d2bdf46c8d5e6934548679
Илья Богданов <ibogdanov@bmstu.ru>
2021-10-09 16:33:27 +0000

    Замечания по Main.cpp

--- 2Lab1/Main.cpp
+++ 2Lab1/Main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-    MeshLoader* My = new AneuMeshLoader;
+    MeshLoader* My = new AneuMeshLoader; //!!! Утечка памяти
     std::string name;
     std::cout << "Enter file name ";
     std::cin >> name;
@@ -14,12 +14,12 @@ int main()
     std::vector<FiniteElement> finite_elements = My->get_finite_elems();
     std::vector<BoundaryFiniteElement> boundaries = My->get_boundary();
 
-    std::vector<int> elems_by_id = My->find_elem_id(1, 2, 3);
-    std::vector<int> elems_by_edge= My->find_elem_edge(1, 2);
+    std::vector<int> elems_by_id = My->find_elem_id(1, 2, 3); //!!! Лишнее копирование
+    std::vector<int> elems_by_edge= My->find_elem_edge(1, 2); //!!! Лишнее копирование
 
-    std::vector<int> node_by_boundary = My->get_node_by_boundary(1);
-    std::vector<int> node_by_finite = My->get_node_by_finite(1);
-    std::vector<int> f_node_by_boundary = My->get_f_node_by_boundary(1);
+    std::vector<int> node_by_boundary = My->get_node_by_boundary(1);  //!!! Лишнее копирование
+    std::vector<int> node_by_finite = My->get_node_by_finite(1); //!!! Лишнее копирование
+    std::vector<int> f_node_by_boundary = My->get_f_node_by_boundary(1); //!!! Лишнее копирование
 
     My->insert_middle();
 }
